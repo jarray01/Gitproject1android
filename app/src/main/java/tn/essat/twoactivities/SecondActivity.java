ff71e6c8ad6f84f97dc17c82b0227725b87057f9
JARRAY <jarraynoureddine@gmail.com>
2021-10-18 10:43:54 +0200

    Initial commit

---
+++ app/src/main/java/tn/essat/twoactivities/SecondActivity.java
@@ -0,0 +1,28 @@
+package tn.essat.twoactivities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY =
+            "com.example.android.twoactivities.extra.REPLY";
+    private EditText mReply;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+
+
+    }
+
+
+}
